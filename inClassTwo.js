ae9ffb0a34f61286dcfae39a6f6a3d7838ded904
kaitlynanderson <kca19@pitt.edu>
2017-03-28 20:57:02 -0400

    Add files via upload

---
+++ inClassTwo.js
@@ -0,0 +1,17 @@
+alert("Prepare For Cuteness");
+
+//This was taken from the book
+
+$(document).ready(function(){
+  $("#accordion").accordion({
+    event: "mouseover",
+    heightStyle: "content",
+    collapsible: true
+  });
+});
+
+//Also from the book
+
+$(document) .ready(function(){
+  $("#tabs").tabs();
+});
